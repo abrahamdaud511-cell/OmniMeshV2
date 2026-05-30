4a2fbffa0e1ddba7fa4b8442e46fdf816e670cde
development <abrahamdaud511@gmail.com>
2026-05-30 11:56:18 +0700

    Initialize omnimesh module with version and exports

---
+++ omnimesh/__init__.py
@@ -0,0 +1,6 @@
+from .model import OmniMeshV2
+from .config import ModelConfig
+from .safety import ConstitutionalSafetyRouterV2
+
+__version__ = "2.0.0"
+__all__ = ["OmniMeshV2", "ModelConfig", "ConstitutionalSafetyRouterV2"]
